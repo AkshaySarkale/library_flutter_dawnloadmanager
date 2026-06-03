03adc89638a8f4330f77d448523fd023bfa51c6e
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-03 09:55:53 +0530

    added android file dawnload logic

--- lib/dawnloadmanager/dawnloadmanagerwithnotification.dart
+++ lib/dawnloadmanager/dawnloadmanagerwithnotification.dart
@@ -1,14 +1,42 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+
 class Dawnloadmanagerwithnotification extends StatefulWidget {
-  const Dawnloadmanagerwithnotification({super.key});
+  final String dawnloadPath;
+  final String dawnloadUrl;
+
+  const Dawnloadmanagerwithnotification({
+    super.key,
+    this.dawnloadPath = "/storage/emulated/0/Dawnload/",
+    required this.dawnloadUrl,
+  });
 
   @override
-  State<Dawnloadmanagerwithnotification> createState() => _DawnloadmanagerwithnotificationState();
+  State<Dawnloadmanagerwithnotification> createState() =>
+      _DawnloadmanagerwithnotificationState();
 }
 
 class _DawnloadmanagerwithnotificationState extends State<Dawnloadmanagerwithnotification> {
+  void dawnloadFIle() async {
+    var time = DateTime.now().microsecondsSinceEpoch;
+    var path = "${widget.dawnloadPath}";
+    var file = File(path);
+    var res = await http.get(Uri.parse(widget.dawnloadUrl));
+    file.writeAsBytes(res.bodyBytes);
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: IconButton(
+        onPressed: () {
+          setState(() {
+            dawnloadFIle();
+          });
+        },
+        icon: Icon(Icons.filter_list_sharp),
+      ),
+    );
   }
 }
