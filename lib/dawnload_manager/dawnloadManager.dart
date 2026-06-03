24d5ef0df4d5b7ab78a559dd8bbbc3fc705e2b54
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-03 16:58:24 +0530

    dawnload funcionally

--- lib/dawnload_manager/dawnloadManager.dart
+++ lib/dawnload_manager/dawnloadManager.dart
@@ -1,78 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
 
-class DownloadManager extends StatefulWidget {
-  final String url;
-
-  const DownloadManager({
-    super.key,
-    required this.url,
-  });
-
-  @override
-  State<DownloadManager> createState() => _DownloadManagerState();
-}
-
-class _DownloadManagerState extends State<DownloadManager> {
-  double? progress;
-
-  Future<void> fileDownload() async {
+class DownloadManager {
+  static Future<void> download({
+    required String url,
+    String? fileName,
+    Function()? onStarted,
+    Function(double progress)? onProgress,
+    Function(String path)? onCompleted,
+    Function(String error)? onFailed,
+  }) async {
     try {
-      FileDownloader.downloadFile(
-        url: widget.url.trim(),
+      onStarted?.call();
 
-        onProgress: (fileName, progressValue) {
-          setState(() {
-            progress = progressValue;
-          });
+      FileDownloader.downloadFile(
+        url: url,
+
+        name: fileName,
+
+        onProgress: (fileName, progress) {
+          onProgress?.call(progress);
         },
 
         onDownloadCompleted: (path) {
-          debugPrint("Downloaded File: $path");
-
-          setState(() {
-            progress = null;
-          });
-
-          // TODO:
-          // Show notification
-          // Open file
+          onCompleted?.call(path);
         },
 
-        onDownloadError: (errorMessage) {
-          debugPrint(errorMessage);
+        onDownloadError: (error) {
+          onFailed?.call(error.toString());
         },
       );
     } catch (e) {
-      debugPrint(e.toString());
+      onFailed?.call(e.toString());
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: fileDownload,
-          child: const Text("Download"),
-        ),
-
-        if (progress != null)
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                LinearProgressIndicator(
-                  value: progress,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "${(progress! * 100).toStringAsFixed(0)}%",
-                ),
-              ],
-            ),
-          ),
-      ],
-    );
   }
 }
