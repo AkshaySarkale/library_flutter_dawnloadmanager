49d625ad9ed66d07bf629804cc379f0813348497
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-03 11:09:57 +0530

    added dawnload manager

--- lib/dawnloadmanager/dawnload_manager.dart
+++ lib/dawnloadmanager/dawnload_manager.dart
@@ -1,6 +1,34 @@
-library download_manager_plus;
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
-export 'src/controller/dawnload_controller.dart';
-export 'src/widgets/dawnload_button.dart';
-export 'src/model/dawnload_task.dart';
-export 'src/model/dawnload_status.dart';
+class DownloadManager {
+  static Future<void> download({
+    required String url,
+    required String savePath,
+    VoidCallback? onStarted,
+    Function(double)? onProgress,
+    VoidCallback? onCompleted,
+    Function(String)? onFailed,
+  }) async {
+    try {
+      onStarted?.call();
+
+      final response = await http.get(Uri.parse(url));
+
+      final file = File(savePath);
+
+      if (!await file.parent.exists()) {
+        await file.parent.create(recursive: true);
+      }
+
+      await file.writeAsBytes(response.bodyBytes);
+
+      onProgress?.call(100);
+
+      onCompleted?.call();
+    } catch (e) {
+      onFailed?.call(e.toString());
+    }
+  }
+}
